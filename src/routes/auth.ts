ef9bdff71373840d1f3e0de8bf1c3c6a44b01138
Phil Saam <philsaam@gmail.com>
2021-01-26 19:10:46 -0500

    login cookie setting

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,16 @@
-import { validate } from "class-validator";
+import { isEmpty, validate } from "class-validator";
 import { Request, Response, Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 import { User } from "../entities/User";
+import { env } from "process";
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
-
   try {
-    // TODO Validate data
+    // s Validate data
     let errors: any = {};
     const emailUser = await User.findOne({ email });
     const usernameUser = await User.findOne({ username });
@@ -18,13 +21,13 @@ const register = async (req: Request, res: Response) => {
     if (Object.keys(errors).length > 0) {
       return res.status(400).json(errors);
     }
-    //TODO Create user
+    //s Create user
     const user = new User({ email, username, password });
     errors = await validate(user);
     if (errors.length > 0) return res.status(400).json({ errors });
     await user.save();
 
-    //TODO Return the user
+    //s Return the user
     return res.json(user);
   } catch (err) {
     console.log(err);
@@ -32,7 +35,65 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(401).json({ password: "Password is incorrect" });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET!);
+
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3600,
+        path: "/",
+      })
+    );
+
+    return res.json(user);
+  } catch (err) {}
+};
+
+const me = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) throw new Error("Unauthenticated");
+
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ username });
+
+    if (!user) throw new Error("Unauthenticated");
+
+    return res.json({ message: "testing" });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ error: err.message });
+  }
+};
+
 const router = Router();
 router.post("/register", register);
-
+router.post("/login", login);
+router.get("/me", me);
 export default router;
