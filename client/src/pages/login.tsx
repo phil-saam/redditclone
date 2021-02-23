dd0885a26bd7fa800e1480898ec4b34f4f877e07
Phil <phil@gmail.com>
2021-02-22 19:28:20 -0500

    login and register pages

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -6,8 +6,7 @@ import { useRouter } from "next/router";
 
 import InputGroup from "../components/InputGroup";
 
-export default function Register() {
-  const [email, setEmail] = useState("");
+export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [agreement, setAgreement] = useState(false);
@@ -17,17 +16,13 @@ export default function Register() {
 
   const submitForm = async (event: FormEvent) => {
     event.preventDefault();
-    if (!agreement) {
-      setErrors({ ...errors, agreement: "you must agree to T&Cs" });
-      return;
-    }
+
     try {
-      await axios.post("/auth/register", {
-        email,
-        password,
+      await axios.post("/auth/login", {
         username,
+        password,
       });
-      router.push("/login");
+      router.push("/");
     } catch (err) {
       setErrors(err.response.data);
     }
@@ -35,7 +30,7 @@ export default function Register() {
   return (
     <div className="flex">
       <Head>
-        <title>Register</title>
+        <title>Login</title>
       </Head>
       <div
         className="h-screen bg-center bg-cover w-36"
@@ -43,34 +38,11 @@ export default function Register() {
       ></div>
       <div className="flex flex-col justify-center pl-6">
         <div className="w-70">
-          <h1 className="mb-2 text-lg font-medium">Sign Up</h1>
+          <h1 className="mb-2 text-lg font-medium">Login</h1>
           {/* <p className="mb-10 text-xs">
             By continuing, you agree to our User Agreement and Privacy Policy.
           </p> */}
           <form onSubmit={submitForm}>
-            <div className="mb-6">
-              <input
-                type="checkbox"
-                className="mr-1 cursor-pointer"
-                id="agreement"
-                checked={agreement}
-                onChange={(e) => setAgreement(e.target.checked)}
-              />
-              <label htmlFor="agreement" className="text-xs cursor-pointer">
-                I agree to the User Agreement and Privacy Policy
-              </label>
-              <small className="block font-medium text-red-600">
-                {errors.agreement}
-              </small>
-            </div>
-            <InputGroup
-              className="mb-2"
-              value={email}
-              placeholder="EMAIL"
-              type="email"
-              setValue={setEmail}
-              error={errors.email}
-            />
             <InputGroup
               className="mb-2"
               value={username}
@@ -87,15 +59,14 @@ export default function Register() {
               setValue={setPassword}
               error={errors.password}
             />
-
             <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded y-2">
-              Sign Up
+              Login
             </button>
           </form>
           <small>
-            Already a redditor?{" "}
-            <Link href="/login">
-              <a className="ml-1 text-blue-500 uppercase">log in</a>
+            New to Reddit?
+            <Link href="/register">
+              <a className="ml-1 text-blue-500 uppercase">Sign Up</a>
             </Link>
           </small>
         </div>
